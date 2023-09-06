083f762e8a73bc5072550d142dfb2e909e521138
João Pedro Macedo <joaopedromacedomonteiro@gmail.com>
2023-09-06 10:22:59 -0300

    Add target, no cargo rinha

---
+++ rinha/target/debug/deps/rinha.d
@@ -0,0 +1,5 @@
+C:\Users\joao\Documents\projeto_pessoal\rinha\target\debug\deps\rinha.exe: src\main.rs
+
+C:\Users\joao\Documents\projeto_pessoal\rinha\target\debug\deps\rinha.d: src\main.rs
+
+src\main.rs:
---
+++ rinha/target/debug/rinha.d
@@ -0,0 +1,1 @@
+C:\Users\joao\Documents\projeto_pessoal\rinha\target\debug\rinha.exe: C:\Users\joao\Documents\projeto_pessoal\rinha\src\main.rs
